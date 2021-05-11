4337ac7f30e9547b84663b6833ca23532b0c0c80
rociomarcel <82036217+rociomarcel@users.noreply.github.com>
2021-05-10 21:09:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/firefox-icon.png') {
+      miImage.setAttribute('src','images/firefox2.png');
+    } else {
+      miImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector( 'h1');
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Mozilla es genial,' + miNombre;
+}
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    let nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Mozilla es genial,' + nombreAlmacenado;
+}
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
